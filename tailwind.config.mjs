2b09e5e9253ab3c382145f0ac6a85152b9920072
esfox <osairis100@gmail.com>
2024-03-31 00:17:01 +0800

    Update content and styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,30 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Gotham', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'primary-foreground': 'var(--color-primary-foreground)',
+        'text-secondary': 'var(--color-text-secondary)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        emerald: {
+          ...require('daisyui/src/theming/themes')['emerald'],
+          primary: '#d19b41',
+          'primary-content': '#fff',
+          secondary: '#0b3522',
+        },
+      },
+    ],
+  },
 };
